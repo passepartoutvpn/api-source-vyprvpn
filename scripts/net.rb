96a80f23c3df0276be495a4914b3d31deb552202
Davide De Rosa <keeshux@gmail.com>
2019-04-26 12:28:19 +0200

    Initial commit

---
+++ scripts/net.rb
@@ -0,0 +1,84 @@
+require "json"
+require "resolv"
+require "ipaddr"
+
+cwd = File.dirname(__FILE__)
+Dir.chdir(cwd)
+
+###
+
+servers = File.foreach("../template/servers.csv")
+ca = File.read("../static/ca.crt")
+domain = "vpn.goldenfrog.com"
+
+cfg = {
+    ca: ca,
+    frame: 1,
+    ping: 10,
+    eku: true
+}
+
+external = {
+    hostname: "${id}.#{domain}"
+}
+
+recommended_cfg = cfg.dup
+recommended_cfg["ep"] = ["UDP:443"]
+recommended_cfg["cipher"] = "AES-256-GCM"
+
+recommended = {
+    id: "default",
+    name: "Default",
+    comment: "256-bit encryption",
+    cfg: recommended_cfg,
+    external: external
+}
+presets = [recommended]
+
+defaults = {
+    :username => "user@mail.com",
+    :pool => "us",
+    :preset => "default"
+}
+
+###
+
+pools = []
+servers.with_index { |line, n|
+    id, country, area = line.strip.split(",")
+    id = id.downcase
+    hostname = "#{id}.#{domain}"
+
+    addresses = nil
+    if ARGV.include? "noresolv"
+        addresses = []
+        #addresses = ["1.2.3.4"]
+    else
+        addresses = Resolv.getaddresses(hostname)
+    end
+    addresses.map! { |a|
+        IPAddr.new(a).to_i
+    }
+
+    pool = {
+        :id => id,
+        :country => country.upcase
+    }
+    if !area.nil?
+        pool[:area] = area
+    end
+    pool[:hostname] = hostname
+    pool[:addrs] = addresses
+    pools << pool
+}
+
+###
+
+infra = {
+    :pools => pools,
+    :presets => presets,
+    :defaults => defaults
+}
+
+puts infra.to_json
+puts
---
+++ update-servers.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+URL="https://support.goldenfrog.com/hc/en-us/articles/360011055671-What-are-the-VyprVPN-server-addresses-"
+TPL="template"
+SERVERS_SRC="$TPL/servers.html"
+SERVERS_DST="$TPL/servers.csv"
+
+mkdir -p $TPL
+if ! curl -L $URL >$SERVERS_SRC.tmp; then
+    exit
+fi
+mv $SERVERS_SRC.tmp $SERVERS_SRC
+sed -nE "s/^.*width: 263\.15px;\">.+ - ([^,]+).*<\/td>$/\1/p" $SERVERS_SRC >names.out
+sed -nE "s/^.*;\">([^0-9]+)([0-9]*)\.vpn\.goldenfrog\.com<.*$/\1\2,\1/p" $SERVERS_SRC >ids.out
+paste -d ',' ids.out names.out >$SERVERS_DST
+rm *.out
+
+# fix codes inconsistencies
+sed -i"" -E "s/,uk,/,gb,/" $SERVERS_DST
+
+sort $SERVERS_DST >$SERVERS_DST.tmp
+mv $SERVERS_DST.tmp $SERVERS_DST
