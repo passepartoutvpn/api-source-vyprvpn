252f9b41c704735c6e917fbc34edcc6853f8fd5a
Davide De Rosa <keeshux@gmail.com>
2022-03-25 09:34:46 +0100

    Upgrade to v5

--- scripts/net.rb
+++ scripts/net.rb
@@ -7,79 +7,75 @@ Dir.chdir(cwd)
 
 ###
 
-servers = File.foreach("../template/servers.csv")
+template = File.foreach("../template/servers.csv")
 ca = File.read("../static/ca.crt")
 domain = "vpn.goldenfrog.com"
 
 cfg = {
-    ca: ca,
-    frame: 1,
-    compression: 1,
-    ping: 10,
-    pingTimeout: 60,
-    eku: false
+  ca: ca,
+  cipher: "AES-256-GCM",
+  compressionFraming: 1,
+  compressionAlgorithm: 1,
+  keepAliveSeconds: 10,
+  keepAliveTimeoutSeconds: 60,
+  checksEKU: false
 }
-
-external = {
-    hostname: "${id}.#{domain}"
-}
-
-recommended_cfg = cfg.dup
-recommended_cfg["ep"] = ["UDP:443"]
-recommended_cfg["cipher"] = "AES-256-GCM"
 
 recommended = {
-    id: "default",
-    name: "Default",
-    comment: "256-bit encryption",
-    cfg: recommended_cfg,
-    external: external
+  id: "default",
+  name: "Default",
+  comment: "256-bit encryption",
+  ovpn: {
+    cfg: cfg,
+    endpoints: [
+      "UDP:443"
+    ]
+  }
 }
 presets = [recommended]
 
 defaults = {
-    :username => "user@mail.com",
-    :pool => "us",
-    :preset => "default"
+  :username => "user@mail.com",
+  :country => "US"
 }
 
 ###
 
-pools = []
-servers.with_index { |line, n|
-    id, country, area = line.strip.split(",")
-    id = id.downcase
-    hostname = "#{id}.#{domain}"
+servers = []
+template.with_index { |line, n|
+  id, country, area = line.strip.split(",")
+  id = id.downcase
+  hostname = "#{id}.#{domain}"
 
-    addresses = nil
-    if ARGV.include? "noresolv"
-        addresses = []
-        #addresses = ["1.2.3.4"]
-    else
-        addresses = Resolv.getaddresses(hostname)
-    end
-    addresses.map! { |a|
-        IPAddr.new(a).to_i
-    }
+  addresses = nil
+  if ARGV.include? "noresolv"
+    addresses = []
+    #addresses = ["1.2.3.4"]
+  else
+    addresses = Resolv.getaddresses(hostname)
+  end
+  addresses.map! { |a|
+    IPAddr.new(a).to_i
+  }
 
-    pool = {
-        :id => id,
-        :country => country.upcase
-    }
-    if !area.nil?
-        pool[:area] = area
-    end
-    pool[:hostname] = hostname
-    pool[:addrs] = addresses
-    pools << pool
+  server = {
+    :id => id,
+    :country => country.upcase
+  }
+  if !area.nil?
+    server[:area] = area
+  end
+  server[:hostname] = hostname
+  server[:addrs] = addresses
+  servers << server
 }
 
 ###
 
 infra = {
-    :pools => pools,
-    :presets => presets,
-    :defaults => defaults
+  :servers => servers,
+  :presets => presets,
+  :defaults => defaults
 }
 
 puts infra.to_json
